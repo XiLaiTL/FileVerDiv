5348ff9e11d3e89aaa2a6dc3ab06bd79aafff3bf
XiLaiTL <1272720210@qq.com>
2023-02-17 13:40:36 +0800

    add comment

--- src/main/java/fun/vari/FileDivider.java
+++ src/main/java/fun/vari/FileDivider.java
@@ -7,44 +7,51 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * 当前读取状态：正在读取模块、普通
+ */
 enum State{
     PART,NONE
 }
+
+/**
+ * 读取文件，对文件进行处理
+ */
 public class FileDivider {
-    public Map<PartModel, List<String>> map = new HashMap<>();
-    public State state = State.NONE;
-    public PartModel commonPart;
-    public PartModel currentPart;
-    public Path filePath;
+    public Map<PartModel, List<String>> map = new HashMap<>(); //存储每个版本的文本信息
+    public State state = State.NONE; //存储当前读取状态
+    public PartModel commonPart; //存储还未拆分时的公共部分
+    public PartModel currentPart; //存储当前读取部分的版本信息
+    public Path filePath; //存储本次拆分的文件
     public FileDivider(String filePathStr){
         filePath = Path.of(filePathStr);
         commonPart = new PartModel("0.0.0","common");
         map.put(commonPart,new ArrayList<>());
-        currentPart = commonPart;
+        currentPart = commonPart; //从公共部分开始
     }
     public void read(){
         try(var in = new BufferedReader(new FileReader(filePath.toFile()))) {
             in.lines().forEach(line->{
-                var result = SentenceInterpreter.typeOneLine(line);
+                var result = SentenceInterpreter.typeOneLine(line); //得到当前行的类型
                 switch (state){
-                    case NONE -> {
+                    case NONE -> { //如果当前读取状态是什么也没读到
                         switch (result){
-                            case BEGIN -> {state=State.PART;}
-                            default -> {map.keySet().forEach(key->map.get(key).add(line));}
+                            case BEGIN -> {state=State.PART;} //读到的行是模块开始，就让读取状态进入模块读取状态
+                            default -> {map.keySet().forEach(key->map.get(key).add(line));} //读到的行是普通行，就塞进整个map里
                         }
                     }
-                    case PART -> {
+                    case PART -> { //进入模块读取状态
                         switch (result) {
                             case BEGIN -> {}
-                            case FIRST, NEXT -> {
-                                var nowPart = SentenceInterpreter.getInfoOneLine(line);
-                                map.put(nowPart, new ArrayList<>(map.get(currentPart)));
+                            case FIRST, NEXT -> { //读取到的行是版本行
+                                var nowPart = SentenceInterpreter.getInfoOneLine(line); //获得行内的版本信息
+                                map.put(nowPart, new ArrayList<>(map.get(commonPart))); //拷贝一份公共内容，事实上应该要拷贝低版本信息？
                                 currentPart = nowPart;
                             }
-                            case NONE -> {map.get(currentPart).add(line);}
-                            case END -> {
-                                state = State.NONE;
-                                currentPart = commonPart;
+                            case NONE -> {map.get(currentPart).add(line);} //读取到普通行，塞进map里
+                            case END -> { //读取到模块结束
+                                state = State.NONE; //设置读取状态进入什么也没读到
+                                currentPart = commonPart; //当前读取部分回到公共部分
                             }
                         }
                     }
@@ -62,8 +69,8 @@ public class FileDivider {
         var filePathParent = filePath.getParent().toString();
         for(var key:map.keySet()){
             var name =  key.fileName().isEmpty()
-                    ? fileName.replace("ver",key.version())
-                    :fileName.replaceAll(".+ver",key.fileName());
+                    ? fileName.replace("ver",key.version()) //将xxx.ver.txt替换为xxx.7.8.9.txt
+                    : fileName.replaceAll(".+ver",key.fileName()); //将xxx.ver.txt替换为 filename.txt
             var NewFilePath = Path.of(filePathParent,fileName);
             try(var out = new BufferedWriter(new BufferedWriter(new FileWriter(NewFilePath.toFile())))) {
                 out.write(String.join("\n",map.get(key)));
--- src/main/java/fun/vari/SentenceInterpreter.java
+++ src/main/java/fun/vari/SentenceInterpreter.java
@@ -1,28 +1,46 @@
 package fun.vari;
 
 import java.util.Arrays;
-
+/**
+ * 存储版本信息和导出时的文本名称
+ */
 record PartModel(String version,String fileName){ }
 
+/**
+ * 判断单行的类型：开始一个模块、首个版本行、版本行、普通行、结束一个模块
+ */
 enum Result{
     BEGIN,FIRST,NEXT,NONE,END
 }
+/**
+ * 对单行进行识别
+ */
 public class SentenceInterpreter {
+    /**
+     * 标定一个行的类型，返回{@link Result}
+     * @param line 单行文本
+     * @return 行的类型
+     */
     public static Result typeOneLine(String line){
-        if(line.matches("^\\s*//main")) return Result.BEGIN;
-        if(line.matches("^\\s*/\\*\\s*@version.+\\*/\\s*$")) return Result.FIRST;
-        if(line.matches("^\\s*/\\*\\s*@version.+")) return Result.NEXT;
-        if(line.matches("^\\s*\\*/\\s*$")) return Result.END;
-        return Result.NONE;
+        if(line.matches("^\\s*//main")) return Result.BEGIN; //识别到//main时，返回模块开始
+        if(line.matches("^\\s*/\\*\\s*@version.+\\*/\\s*$")) return Result.FIRST; //识别到/*@version*/时，返回首个版本行
+        if(line.matches("^\\s*/\\*\\s*@version.+")) return Result.NEXT; //识别到/*@version时，返回版本行
+        if(line.matches("^\\s*\\*/\\s*$")) return Result.END; //识别到*/时，返回模块结束
+        return Result.NONE; //返回普通行
     }
 
+    /**
+     * 获得一个版本行的信息，格式为{@link PartModel}
+     * @param line 单行文本
+     * @return  版本信息和导出时的文本名称
+     */
     public static PartModel getInfoOneLine(String line){
-        var dealLine = line.replaceAll("^\\s*/\\*\\s*","").replaceAll("\\*/\\s*$","");
-        var list = dealLine.split("@");
+        var dealLine = line.replaceAll("^\\s*/\\*\\s*","").replaceAll("\\*/\\s*$",""); //截取出@version xxx @file-name xxx格式
+        var list = dealLine.split("@"); //截取出["","version xxx","file-name xxxx"]
         var version = "";
         var fileName = "";
         for(var term:list){
-            var cell = term.split(" ");
+            var cell = term.split(" "); //截取出["version","xxx"]
             if(cell.length==2){
                 if("version".equalsIgnoreCase(cell[0])){version=cell[1];}
                 if("file-name".equalsIgnoreCase(cell[0])){fileName=cell[1];}
